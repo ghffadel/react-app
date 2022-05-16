9b0f47cb9dd13b2a6ddb6f7db7b001a27411f19d
Luis <lhsda.snf20@uea.edu.br>
2022-05-16 17:04:48 -0400

    Adding Team route

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet } from "react-native";
+import Equipe from "./screens/Equipe";
 import História from "./screens/História";
 import Intro from "./screens/Intro";
 const Stack = createNativeStackNavigator();
@@ -8,9 +9,10 @@ const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Intro">
+      <Stack.Navigator >
         <Stack.Screen name="Intro" component={Intro} options={{headerTitle: "React Native"}}/>
         <Stack.Screen name="História" component={História} options={{headerTitle: "História"}}/>
+        <Stack.Screen name="Equipe" component={Equipe} options={{headerTitle: "Equipe"}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
