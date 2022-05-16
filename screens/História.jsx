a297aa531a8c1d6703994eed7eda053bb181a0bc
Luis <lhsda.snf20@uea.edu.br>
2022-05-16 17:03:32 -0400

    Initial history screen

--- screens/História.jsx
+++ screens/História.jsx
@@ -1,13 +1,17 @@
 import { SafeAreaView } from 'react-native'
+import { Button } from 'react-native-web'
 import styled from 'styled-components/native'
 
 const StyledText = styled.Text`
     color: red;
 `
-const História = () => {
+const História = ({navigation}) => {
   return (
     <SafeAreaView>
       <StyledText>História</StyledText>
+      <Button onPress={() => {
+        navigation.navigate('Equipe')
+      }}/>
     </SafeAreaView>
   )
 }
